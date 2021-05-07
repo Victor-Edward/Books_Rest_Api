8fc79ed4ad27884c83132c38a6a30b5ec2b8dcf8
Victor Edward <victoredward30@gmail.com>
2021-05-06 21:28:30 -0300

    feat: Created the Author and Book models

--- books_api/models.py
+++ books_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField("Nome", max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField("Título", max_length=128)
+    isbn = models.CharField("ISBN", max_length=64)
+    author = models.ManyToManyField(Author, verbose_name="Autor(es)")
+    published_by = models.CharField("Editora", max_length=128)
+    book_edition = models.CharField("Edição", max_length=128)
+    pages = models.IntegerField("Número de páginas")
+    description = models.TextField("Descrição")
+
+    def __str__(self):
+        return self.title
